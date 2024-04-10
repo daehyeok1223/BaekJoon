0a7fe8e2271bba5eea2fe200c87fd410543d773c
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2024-04-10 17:45:05 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -11,11 +11,12 @@ int main(){
   arr[2] = 2;
   arr[3] = 4;
   cin >> T;
-  for (int i = 0; i < T; i++) {
-    cin >> n;
-    for (int j = 4; j <= n; j++){
-        arr[j] = arr[j - 3] + arr[j - 2] + arr[j - 1];
-    }
+  for (int i = 4; i < 11; i++){
+    arr[i] = arr[i - 3] + arr[i - 2] + arr[i - 1];
+  }
+    for (int i = 0; i < T; i++) {
+      cin >> n;
+
     cout << arr[n] << '\n';
   }
 }
