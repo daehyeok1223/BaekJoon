6301f1ad0b0e4fc91340bb06499ce6a95ba75172
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2024-05-11 12:00:06 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int dp[1000001];
+
+int main(){
+  int N;
+  cin >> N;
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 2;
+  for (int i = 3; i <= N; i++){
+    dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+  }
+  cout << dp[N];
+}
