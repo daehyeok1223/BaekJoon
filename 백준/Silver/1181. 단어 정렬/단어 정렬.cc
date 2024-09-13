338fbd04c9b78ba664c2f0c03feaacddac944cf6
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2024-09-13 20:30:24 +0900

    [Silver V] Title: 단어 정렬, Time: 64 ms, Memory: 4108 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+bool compare(string a, string b){
+  if(a.length() == b.length()){
+    return a < b;
+  }
+  else{
+    return a.length() < b.length();
+  }
+}
+
+string word[20000];
+
+int main(){
+  int N;
+  cin >> N;
+  for (int i = 0; i < N; i++){
+    cin >> word[i];
+  }
+  sort(word, word + N, compare);
+
+  for (int i = 0; i < N; i++){
+    if(word[i] == word[i-1]){
+      continue;
+    }
+    cout << word[i] << "\n";
+  }
+}
