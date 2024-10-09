4f911f38db026eb6cd8cf8c060472c717306d2f4
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2024-10-09 17:18:26 +0900

    [Silver III] Title: 소수 구하기, Time: 168 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -5,24 +5,24 @@ using namespace std;
 int main(){
   double n, m;
   cin >> n >> m;
-  
   if(n == 1){
-    n++; 
+    n++;
   }
-
   for (int i = n; i <= m; i++) {
-    int flag = 1; 
+    int flag = 1;
     int k = round(sqrt(i));
-
     for (int j = 2; j <= k; j++) {
       if(i % j == 0){
-        flag = 0; 
+        flag = 0;
         break;
       }
+      else{
+        flag = 1;
+      }
     }
-    
-    if (flag == 1) { 
+    if (flag == 1) {
       cout << i << '\n';
     }
+    flag = 0;
   }
 }
